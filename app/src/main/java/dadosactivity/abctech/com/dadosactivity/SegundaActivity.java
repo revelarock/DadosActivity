1403f5168ac9a40129c531c25c11d09998f59d43
Reginaldo Barros de Sousa <32406036+revelarock@users.noreply.github.com>
2017-11-12 00:37:11 -0200

    Initial commit

---
+++ app/src/main/java/dadosactivity/abctech/com/dadosactivity/SegundaActivity.java
@@ -0,0 +1,30 @@
+package dadosactivity.abctech.com.dadosactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        texto = (TextView) findViewById(R.id.textoId);
+
+        Bundle extra = getIntent().getExtras();
+
+
+        if( extra != null) {
+            String textoPassado = extra.getString("Nome");
+            texto.setText(textoPassado);
+
+        }
+
+
+
+    }
+}
